66ba04cb7d0b2d0a35c8ba3dfa32f24b97da6c6b
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-11-17 23:14:35 +0530

    change color

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -3,10 +3,10 @@ import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.da
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:gemhub/category_screen.dart';
 import 'package:gemhub/login_screen.dart';
-import 'package:gemhub/cart_screen.dart'; // Import Cart Screen
-import 'package:gemhub/profile_screen.dart'; // Import Profile Screen
+import 'package:gemhub/cart_screen.dart';
+import 'package:gemhub/profile_screen.dart';
 import 'package:gemhub/auction_screen.dart';
-import 'package:gemhub/widget/category_card%20.dart'; // Import Auction Screen
+import 'package:gemhub/widget/category_card%20.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -36,13 +36,11 @@ class _HomeScreenState extends State<HomeScreen> {
     });
 
     if (index == 1) {
-      // Navigate to Cart Screen
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => const CartScreen()),
       );
     } else if (index == 3) {
-      // Navigate to Profile Screen
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => const ProfileScreen(name: '', email: '', phone: '',)),
@@ -85,7 +83,7 @@ class _HomeScreenState extends State<HomeScreen> {
       child: Scaffold(
         appBar: AppBar(
           backgroundColor: Colors.white,
-          elevation: 0,
+          elevation: 5,
           title: Row(
             children: [
               Image.asset(
@@ -103,25 +101,28 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: SingleChildScrollView(
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: const EdgeInsets.all(16.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 const Text(
                   'Welcome Rensith,',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(height: 10),
                 TextField(
                   decoration: InputDecoration(
                     prefixIcon: const Icon(Icons.search),
-                    hintText: 'Search',
+                    hintText: 'Search products...',
                     border: OutlineInputBorder(
                       borderRadius: BorderRadius.circular(25),
                     ),
+                    filled: true,
+                    fillColor: const Color.fromARGB(255, 221, 255, 187),
+                    contentPadding: const EdgeInsets.symmetric(vertical: 0),
                   ),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 CarouselSlider(
                   options: CarouselOptions(
                     height: 150.0,
@@ -140,15 +141,21 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   )).toList(),
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 const Text(
                   'Categories',
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(height: 10),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
+                const SizedBox(height: 5),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 3,
+                  childAspectRatio: 0.85,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  padding: const EdgeInsets.all(8),
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: const [
                     CategoryCard(
                       imagePath: 'assets/images/category1.jpg',
                       title: 'Blue Sapphires',
@@ -163,15 +170,21 @@ class _HomeScreenState extends State<HomeScreen> {
                     ),
                   ],
                 ),
-                const SizedBox(height: 20),
+                const SizedBox(height: 15),
                 const Text(
                   'Popular Gems',
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
-                const SizedBox(height: 10),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
+                const SizedBox(height: 5),
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.75,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  padding: const EdgeInsets.all(8),
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: const [
                     ProductCard(
                       imagePath: 'assets/images/gem01.jpg',
                       title: '4.37ct Natural Blue Sapphire',
@@ -195,6 +208,11 @@ class _HomeScreenState extends State<HomeScreen> {
               MaterialPageRoute(builder: (context) => const AuctionScreen()),
             );
           },
+          backgroundColor: const Color.fromARGB(255, 221, 255, 187),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          elevation: 8,
           child: const Icon(Icons.gavel),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
@@ -204,8 +222,110 @@ class _HomeScreenState extends State<HomeScreen> {
           gapLocation: GapLocation.center,
           notchSmoothness: NotchSmoothness.smoothEdge,
           onTap: _onItemTapped,
+          backgroundColor: const Color.fromARGB(255, 221, 255, 187),
+          activeColor: const Color.fromARGB(255, 9, 119, 33),
           leftCornerRadius: 32,
           rightCornerRadius: 32,
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+
+  const CategoryCard({super.key, required this.imagePath, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            boxShadow: [
+              BoxShadow(
+                color: const Color.fromARGB(255, 148, 145, 145).withOpacity(0.3),
+                spreadRadius: 3,
+                blurRadius: 5,
+              ),
+            ],
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          title,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 14,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProductCard extends StatelessWidget {
+  final String imagePath;
+  final String title;
+  final String price;
+
+  const ProductCard({
+    super.key,
+    required this.imagePath,
+    required this.title,
+    required this.price,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 3,
+            blurRadius: 5,
+          ),
+        ],
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: DecorationImage(
+                  image: AssetImage(imagePath),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            const SizedBox(height: 5),
+            Text(
+              title,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 3),
+            Text(
+              price,
+              style: const TextStyle(color: Colors.grey),
+            ),
+          ],
         ),
       ),
     );
