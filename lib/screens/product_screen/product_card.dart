767fb5585ef283041528b91cdc19ec6f00799659
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-02-22 23:21:04 +0530

    feat(product-card): convert to StatefulWidget and add quantity management for cart functionality

--- lib/screens/product_screen/product_card.dart
+++ lib/screens/product_screen/product_card.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class ProductCard extends StatelessWidget {
+class ProductCard extends StatefulWidget {
   final String imagePath;
   final String title;
   final String price;
@@ -13,6 +13,35 @@ class ProductCard extends StatelessWidget {
   });
 
   @override
+  _ProductCardState createState() => _ProductCardState();
+}
+
+class _ProductCardState extends State<ProductCard> {
+  int quantity = 0;
+
+  void _addToCart() {
+    setState(() {
+      quantity = 1;
+    });
+  }
+
+  void _incrementQuantity() {
+    setState(() {
+      quantity++;
+    });
+  }
+
+  void _decrementQuantity() {
+    setState(() {
+      if (quantity > 1) {
+        quantity--;
+      } else {
+        quantity = 0;
+      }
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return InkWell(
       borderRadius: BorderRadius.circular(20),
@@ -21,90 +50,85 @@ class ProductCard extends StatelessWidget {
       },
       child: SizedBox(
         width: 160,
-        child: Stack(
-          children: [
-            // Card content
-            Card(
-              elevation: 8,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+        height: 250,
+        child: Card(
+          elevation: 8,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          shadowColor: Colors.black.withOpacity(0.2),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                child: Image.asset(
+                  widget.imagePath,
+                  width: double.infinity,
+                  height: 100,
+                  fit: BoxFit.cover,
+                ),
               ),
-              shadowColor: Colors.black.withOpacity(0.2),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Image container
-                  ClipRRect(
-                    borderRadius: const BorderRadius.only(
-                      topLeft: Radius.circular(20),
-                      topRight: Radius.circular(20),
-                    ),
-                    child: Image.asset(
-                      imagePath,
-                      width: double.infinity,
-                      height: 150,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  // Title
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        final textStyle = const TextStyle(
+              const SizedBox(height: 8),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        widget.title,
+                        style: const TextStyle(
                           fontWeight: FontWeight.bold,
+                          fontSize: 16,
                           color: Colors.black87,
-                        );
-
-                        final textPainter = TextPainter(
-                          text: TextSpan(text: title, style: textStyle.copyWith(fontSize: 16)),
-                          maxLines: 1,
-                          textDirection: TextDirection.ltr,
-                        );
-
-                        textPainter.layout(maxWidth: constraints.maxWidth);
-
-                        final isOverflowing = textPainter.didExceedMaxLines;
-
-                        return Text(
-                          title,
-                          style: textStyle.copyWith(fontSize: isOverflowing ? 12 : 16),
-                          maxLines: isOverflowing ? 2 : 1,
-                          overflow: TextOverflow.ellipsis,
-                        );
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 4),
-                  // Price
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Text(
-                      price,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.green,
-                        fontWeight: FontWeight.w500,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
                       ),
-                    ),
+                      Text(
+                        widget.price,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.green,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      quantity == 0
+                          ? ElevatedButton(
+                              onPressed: _addToCart,
+                              child: const Text("Add to Cart"),
+                            )
+                          : Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.remove),
+                                  onPressed: _decrementQuantity,
+                                ),
+                                Text(
+                                  "$quantity",
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.add),
+                                  onPressed: _incrementQuantity,
+                                ),
+                              ],
+                            ),
+                    ],
                   ),
-                  const SizedBox(height: 8),
-                ],
+                ),
               ),
-            ),
-            // Heart icon in the bottom right corner
-            // Positioned(
-            //   bottom: 8,
-            //   right: 8,
-            //   child: IconButton(
-            //     icon: const Icon(Icons.favorite_border, color: Colors.red),
-            //     onPressed: () {
-            //       // Handle heart icon press
-            //     },
-            //   ),
-            // ),
-          ],
+              const SizedBox(height: 8),
+            ],
+          ),
         ),
       ),
     );
