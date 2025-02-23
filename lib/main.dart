461318a7a15f4f07a0ece1ae9727612965ff039d
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-02-23 17:18:39 +0530

    feat(auth): initialize Firebase with platform-specific options and add anonymous sign-in method

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gemhub/screens/firebase_options/firebase_options.dart';
 
 import 'splash_screen.dart'; 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(); 
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  ); 
   runApp(MyApp());
 }
 
+Future<void> _signInAnonymously() async {
+  await FirebaseAuth.instance.signInAnonymously();
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
