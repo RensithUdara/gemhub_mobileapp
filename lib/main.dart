9e08ced5a4f1a5b3c5cd1d929aaa3165d11c22ee
RensithUdara <rensithudaragonalagoda@gmail.com>
2025-03-08 13:39:02 +0530

    refactor: adjust import order in main.dart for better organization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:gemhub/screens/firebase_options/firebase_options.dart';
 import 'package:gemhub/screens/cart_screen/cart_provider.dart'; // Import CartProvider
+import 'package:gemhub/screens/firebase_options/firebase_options.dart';
 import 'package:gemhub/splash_screen.dart';
 import 'package:provider/provider.dart';
 
